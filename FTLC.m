b52c4b999b15514613f9eebc8cbfcb168dca4657
mateoquaglia <117778104+mateoquaglia@users.noreply.github.com>
2024-06-24 00:13:10 -0300

    Add files via upload

---
+++ FTLC.m
@@ -0,0 +1,25 @@
+%funcion de transferencia a lazo cerrado
+clc;close all; clear all;
+s=tf('s');
+
+%obtenemos la funcion de transferencia de nuestro sistema
+
+%electrovalvula
+ Fv=(2.542e-4)/(s+0.006208)
+%tanque
+Ft=((25.46)/(25.46*s+1))
+%sensor de nivel
+Fs=((240.7)/(s+19.11))
+
+%por teorema de circuitos retroalimentados y simplificacion obtenemos
+
+FT= ((Fv * Ft)/(1+(Fv *Ft * Fs)))
+FT_simplificada = minreal(FT);
+
+
+%analisis en frecuencia
+bode(FT);
+%respuesta al impulso
+impulse(FT);
+%analisis de error
+step(FT);
