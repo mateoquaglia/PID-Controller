73a1b33a3472d63597bd3926364b58ba445df2bb
mateoquaglia <117778104+mateoquaglia@users.noreply.github.com>
2024-06-29 00:04:57 -0300

    Update planta.m

--- planta.m
+++ planta.m
@@ -1,12 +1,12 @@
 
-L = 1;       % longitud del tubo en metros 
+L = 1;       % simulo la longitud del tubo en metros 
 mu = 0.001;  % viscosidad dinamica del fluido 
 r = 0.1;     % radio del tubo en metros
 C = 1;       % area transversal del tanque
 % aplicamos la ley de Poiseuille para calcular R 
 R_poiseuille = (8 * mu * L) / (pi * r^4);
 
-disp(['La resistencia hidráulica calculada con la ley de Poiseuille es: ' num2str(R_poiseuille) ' Pa·s/m^3']);
+disp(['La resistencia hidrÃ¡ulica calculada con la ley de Poiseuille es: ' num2str(R_poiseuille) ' PaÂ·s/m^3']);
 
 % funcion de transferencia H(s)/Qi(s)
 numerator = R_poiseuille;
