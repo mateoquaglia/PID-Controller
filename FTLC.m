d516cc11b4118cb9538b0988020efc86fe50a6c8
mateoquaglia <117778104+mateoquaglia@users.noreply.github.com>
2024-07-11 20:11:43 -0300

    Update FTLC.m

--- FTLC.m
+++ FTLC.m
@@ -17,7 +17,7 @@ FT= ((Fv * Ft)/(1+(Fv *Ft * Fs)))
 FT_simplificada = minreal(FT);
 
 
-%analisis en frecuencia de FTL
+%analisis en frecuencia de FTLC
 bode(FT);
 %respuesta al impulso
 impulse(FT);
